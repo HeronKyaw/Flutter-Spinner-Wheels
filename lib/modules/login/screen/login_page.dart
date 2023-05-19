fa66b71b142b895d7c35fb3ae4f010a0cf17036e
wunnakyaw <wunnakyaw@globalwave.company>
2023-05-19 16:47:16 +0630

    making login ui looks more pretty

--- lib/modules/login/screen/login_page.dart
+++ lib/modules/login/screen/login_page.dart
@@ -48,14 +48,7 @@ class LoginViewState extends State<LoginView> {
     return Scaffold(
       body: BlocConsumer<LoginBloc, LoginState>(
         listener: (context, state) {
-          if (state is LoginSuccess) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(
-                content: Text("Login Success"),
-                backgroundColor: Colors.green,
-              ),
-            );
-          } else if (state is LoginFailure) {
+          if (state is LoginFailure) {
             ScaffoldMessenger.of(context).showSnackBar(
               const SnackBar(
                 content: Text("LOGIN FAILURE"),
@@ -81,70 +74,73 @@ class LoginViewState extends State<LoginView> {
                         child: Text('Something went wrong!'),
                       );
                     }else {
-                      return Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          const SizedBox(height: 64),
-                          const Text(
-                            'Hi, Wecome Back! 👋',
-                            style: TextStyle(
-                              fontSize: 25,
-                              fontWeight: FontWeight.w600,
+                      return Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            const SizedBox(height: 64),
+                            const Text(
+                              'Hi, Wecome Back! 👋',
+                              style: TextStyle(
+                                fontSize: 25,
+                                fontWeight: FontWeight.w600,
+                              ),
                             ),
-                          ),
-                          const Text(
-                            'Hello again, you’ve been missed!',
-                            style: TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w600,
-                              color: Color.fromRGBO(153, 158, 161, 1)
+                            const Text(
+                              'Hello again, you’ve been missed!',
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w600,
+                                color: Color.fromRGBO(153, 158, 161, 1)
+                              ),
                             ),
-                          ),
-                          const Gap(46),
-                          LoginTextField(
-                            title: 'Email',
-                            hintText: 'Please enter your email',
-                            controller: _userNameController,
-                            obscure: false,
-                          ),
-                          const Gap(15),
-                          LoginTextField(
-                            title: 'Password',
-                            hintText: 'Enter Password',
-                            controller: _passwordController,
-                            obscure: true,
-                          ),
-                          const Gap(20),
-                          ElevatedButton(
-                            onPressed: () async{
-                              showDialog(
-                                context: context, 
-                                builder: (context) => const Center(
-                                  child: CircularProgressIndicator(),
+                            const Gap(46),
+                            LoginTextField(
+                              title: 'Email',
+                              hintText: 'Please enter your email',
+                              controller: _userNameController,
+                              obscure: false,
+                            ),
+                            const Gap(15),
+                            LoginTextField(
+                              title: 'Password',
+                              hintText: 'Enter Password',
+                              controller: _passwordController,
+                              obscure: true,
+                            ),
+                            const Gap(20),
+                            ElevatedButton(
+                              onPressed: () async{
+                                showDialog(
+                                  context: context, 
+                                  builder: (context) => const Center(
+                                    child: CircularProgressIndicator(),
+                                  )
+                                );
+                                context.read<LoginBloc>().add(
+                                  LoginButtonPressed(
+                                    username: _userNameController.text,
+                                    password: _passwordController.text,
+                                  ),
+                                );
+                                // remove circular animation cuz Navigator.of(context).pop() does not work.
+                                navigatorKey.currentState!.popUntil((route) => route.isFirst);
+                              },
+                              style: ButtonStyle(
+                                textStyle: MaterialStateProperty.all<TextStyle>(const TextStyle(fontSize: 16)),
+                                padding: MaterialStateProperty.all<EdgeInsets>(const EdgeInsets.all(15)),
+                                backgroundColor: MaterialStateProperty.all<Color>(const Color.fromRGBO(78, 1, 137, 1)),
+                                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                  )
                                 )
-                              );
-                              context.read<LoginBloc>().add(
-                                LoginButtonPressed(
-                                  username: _userNameController.text,
-                                  password: _passwordController.text,
-                                ),
-                              );
-                              // remove circular animation cuz Navigator.of(context).pop() does not work.
-                              navigatorKey.currentState!.popUntil((route) => route.isFirst);
-                            },
-                            style: ButtonStyle(
-                              textStyle: MaterialStateProperty.all<TextStyle>(const TextStyle(fontSize: 16)),
-                              padding: MaterialStateProperty.all<EdgeInsets>(const EdgeInsets.all(15)),
-                              backgroundColor: MaterialStateProperty.all<Color>(const Color.fromRGBO(78, 1, 137, 1)),
-                              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                                RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10.0),
-                                )
-                              )
+                              ),
+                              child: const Text('Login'),
                             ),
-                            child: const Text('Login'),
-                          ),
-                        ],
+                          ],
+                        ),
                       );
                     }
                   }
